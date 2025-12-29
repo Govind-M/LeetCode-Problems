b49266a907b853bbd1897c0c25c36b8e06f33632
Govind M <81026010+Govind-M@users.noreply.github.com>
2025-12-30 00:08:13 +0530

    Time: 1 ms (96.33%), Space: 56.1 MB (91.11%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var nextGreaterElement = function (nums1, nums2) {
+    let map = {}
+    let stack = []
+    let ans = []
+
+    stack.push(nums2[nums2.length - 1])
+    map[nums2[nums2.length - 1]] = -1
+
+    for (let i = nums2.length - 2; i >= 0; i--) {
+        if (nums2[i] < stack[stack.length - 1]) {
+            map[nums2[i]] = stack[stack.length - 1]
+        }
+        else {
+            while (stack.length) {
+                if (nums2[i] > stack[stack.length - 1]) {
+                    stack.pop()
+                }
+                else {
+                    map[nums2[i]] = stack[stack.length - 1]
+                    break
+                }
+            }
+
+            if (!stack.length) {
+                map[nums2[i]] = -1
+            }
+        }
+        stack.push(nums2[i])
+    }
+
+    for (let j = 0; j < nums1.length; j++) {
+        ans.push(map[nums1[j]])
+    }
+
+    return ans
+};
