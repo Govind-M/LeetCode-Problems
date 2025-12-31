6b0c84fbdd0fcfc61a0e6cff9aef4d635083457e
Govind M <81026010+Govind-M@users.noreply.github.com>
2025-12-31 20:04:18 +0530

    Time: 53 ms (15.06%), Space: 76.1 MB (75.9%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures = function (t) {
+    let ans = []
+    let stack = []
+
+    ans[t.length-1] = 0
+    stack.push(t.length - 1)
+
+    for (let i = t.length - 2; i >= 0; i--) {
+        while (stack.length) {
+            if (t[i] >= t[stack[stack.length - 1]]) {
+                stack.pop()
+            }
+            else
+            {
+                ans[i] = stack[stack.length-1] - i
+                break
+            }
+        }
+
+        if(stack.length === 0){
+            ans[i] = 0
+        }
+
+        stack.push(i)
+    }
+    return ans
+};
