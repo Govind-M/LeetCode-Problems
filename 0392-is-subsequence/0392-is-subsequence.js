b78ee2c7ebd5f1dcc2cd208ac83a3bd867f1b9bb
Govind M <81026010+Govind-M@users.noreply.github.com>
2026-01-28 20:09:17 +0530

    Time: 0 ms (100%), Space: 54.7 MB (68.41%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.js
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -7,14 +7,14 @@ var isSubsequence = function (s, t) {
     let i = 0
     let j = 0
 
-    while (j <= t.length) {
+    while (j < t.length) {
         if (s[i] == t[j]) {
             i += 1
         }
         j += 1
     }
 
-    if(i>s.length){
+    if(i==s.length){
         return true
     }
     else{
