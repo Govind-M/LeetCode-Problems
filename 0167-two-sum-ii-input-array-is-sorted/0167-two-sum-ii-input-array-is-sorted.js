a5a189b07a1a29740e937f19595e1f1dbed6b7b7
Govind M <81026010+Govind-M@users.noreply.github.com>
2026-01-28 18:37:29 +0530

    Time: 1 ms (82.54%), Space: 57.2 MB (8.49%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (arr, target) {
+    let i = 0
+    let j = arr.length - 1
+
+    while (i < j) {
+        let sum = arr[i] + arr[j]
+
+        if (sum > target) {
+            j -= 1
+        }
+        else if (sum < target) {
+            i += 1
+        }
+        else {
+            return [i + 1, j + 1]
+        }
+    }
+};
