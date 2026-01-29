834c2a95d069ccaa8006a3473ff1efe5c819423d
Govind M <81026010+Govind-M@users.noreply.github.com>
2026-01-29 20:25:47 +0530

    Time: 0 ms (100%), Space: 55.1 MB (21.19%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+    let m = haystack.length
+    let n = needle.length
+    for (let i = 0; i <= m - n; i++) {
+        let j = 0
+        for (j = 0; j < n; j++) {
+            if (haystack[i + j] !== needle[j]) {
+                break;
+            }
+        }
+        if (j === n) {
+            return i
+        }
+    }
+    return -1
+};
