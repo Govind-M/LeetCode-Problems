e92d1ed8c45c8f9050fc8fe1aa4c86ad8eb4ec90
Govind M <81026010+Govind-M@users.noreply.github.com>
2026-02-09 22:54:21 +0530

    Time: 33 ms (79.1%), Space: 72.9 MB (42.88%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -3,32 +3,36 @@
  * @return {number[][]}
  */
 
-var threeSum = function(nums) {
-    nums.sort((a, b) => a - b);
-    let ans = [];
-    for (let i = 0; i < nums.length; i++) {
-        if (i === 0 || nums[i] !== nums[i - 1]) {
-            twoSum(nums, i, ans);
+
+var threeSum = function (arr) {
+    arr.sort((a, b) => a - b)
+    let ans = []
+    for (let i = 0; i < arr.length; i++) {
+        if (i===0 || arr[i] != arr[i - 1]) {
+            twoSum(arr, i, ans)
         }
     }
-    return ans;
+    return ans
 };
-var twoSum = function(arr, x, ans) {
-    let i = x + 1;
-    let j = arr.length - 1;
+
+var twoSum = function (arr, x, ans) {
+    let i = x + 1
+    let j = arr.length - 1
+
     while (i < j) {
-        let sum = arr[i] + arr[j] + arr[x];
+        let sum = arr[x] + arr[i] + arr[j]
         if (sum > 0) {
-            j--;
-        } else if (sum < 0) {
-            i++;
-        } else {
-            ans.push([arr[i], arr[j], arr[x]]);
-            i++;
-            j--;
-            // Skip duplicates for the second element
-            while (i < j && arr[i] === arr[i - 1]) {
-                i++;
+            j -= 1
+        }
+        else if (sum < 0) {
+            i += 1
+        }
+        else {
+            ans.push([arr[x], arr[i], arr[j]])
+            i += 1
+            j -= 1
+            while (i < j && arr[i] === arr[i - 1]){
+                i+=1
             }
         }
     }
