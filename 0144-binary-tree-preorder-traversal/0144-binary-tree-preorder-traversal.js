547b4cc564e59aaa0315fb827df882f3aa0f6a12
Govind M <81026010+Govind-M@users.noreply.github.com>
2026-03-03 23:48:52 +0530

    Time: 1 ms (4.8%), Space: 54.1 MB (39.24%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.js
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.js
@@ -10,16 +10,38 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
+// var preorderTraversal = function(root) {
+
+//     let ans = []
+//     function traversal(curr){
+//         if(!curr) return
+//         ans.push(curr.val)
+//         traversal(curr.left)
+//         traversal(curr.right)
+//     }
+
+//     traversal(root)
+//     return ans
+// };
+
 var preorderTraversal = function(root) {
 
-    let ans = []
-    function traversal(curr){
-        if(!curr) return
-        ans.push(curr.val)
-        traversal(curr.left)
-        traversal(curr.right)
-    }
+let stack = []
+let ans = []
 
-    traversal(root)
-    return ans
+stack.push(root)
+
+while(stack.length){
+
+if(!root) return []
+
+let curr = stack.pop()
+ans.push(curr.val) 
+curr.right && stack.push(curr.right)
+curr.left && stack.push(curr.left)
+}
+
+return ans
+
 };
+
